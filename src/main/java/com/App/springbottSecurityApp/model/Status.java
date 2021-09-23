cc6ed8f805f03f717840b97fa88a8a23bfe3cc30
SergeyStupak <stupak.sergey@mail.ru>
2021-09-24 00:17:39 +0300

    commit

---
+++ src/main/java/com/App/springbottSecurityApp/model/Status.java
@@ -0,0 +1,5 @@
+package com.App.springbottSecurityApp.model;
+
+public enum Status {
+    ACTIVE, BANNED
+}
---
+++ src/test/java/com/App/springbottSecurityApp/SpringbottSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.App.springbottSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbottSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
